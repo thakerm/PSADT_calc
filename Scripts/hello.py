6348a8d8acd8d37bfcf3d401e9f23baa868c0a40
Gautam Thaker <ghthaker@gmail.com>
2022-04-20 09:31:26 -0700

    Need to use natural (base 'e') log, but Mihir's code so he needs to be sure.

--- Scripts/hello.py
+++ Scripts/hello.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 x = [32, 1]
 y = [6, 2.2]
-y_ln = np.log2(y)
+y_ln = np.log(y)   # natural log (base 'e')
 print(y_ln)
-slope = stats.linregress(x,y_ln)
-slopes = 1/slope[0]
+slope = stats.linregress(x,y_ln)  # slope is tuple (m, b) for m*x+b line
+slopes = np.log(2)/slope[0]
 print(slopes)
 
