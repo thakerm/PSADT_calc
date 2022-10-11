1ff8e7744d195d01b8f512dee7f31bc33c8a619a
Gautam Thaker <ghthaker@gmail.com>
2022-10-11 08:24:26 -0400

    Remove these older, not used things that were only used in early dev work.

--- Scripts/slope.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-x = [4, 8]
-y = [5, 10]
-slope, intercept = np.polyfit(x,y,1)
-print(slope)
--- Scripts/string_test.js
+++
@@ -1,4 +0,0 @@
-var string = "This is a test";
-var string_split = string.split(" ");
-
-console.log(string_split);
